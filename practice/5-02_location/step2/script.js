82d0f129515d453632137c9c0d577b30d82d9790
taimushimizu <213.f.r48132@gmail.com>
2020-09-27 01:34:17 +0900

    5-02の終了

---
+++ practice/5-02_location/step2/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const lang= document.querySelector('html').lang;
+
+if(lang === 'ja'){
+  document.querySelector('option[value="index.html"]').selected = true;
+} else if (lang === 'en'){
+  document.querySelector('option[value="index-en.html"]').selected = true;
+} else if (lang === 'zh'){
+  document.querySelector('option[value="index-zh.html"]').selected = true;
+}
+
+switch (lang) {
+  case 'ja':
+    document.querySelector('option[value="index.html"]').selected = true;
+    break;
+  case 'en':
+    document.querySelector('option[value="index-en.html"]').selected = true;
+    break;
+  case 'zh':
+    document.querySelector('option[value="index-zh.html"]').selected = true;
+    break;
+}
+
+document.getElementById('form').select.onchange = function(){
+  location.href = document.getElementById('form').select.value;
+}
